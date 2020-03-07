16eec292f4d4edc5a1abad64b0a25f018100c648
Goh Yon Cing <55395418+Yoncing99@users.noreply.github.com>
2020-03-07 22:34:41 +0800

    Update output.java

--- output.java
+++ output.java
@@ -1,9 +1,6 @@
-// String hi = new String();
-// String hi = ("Are you okay?");
-
 public class output{
     public static void main(String[]args){
         String phr = new String("Big, blue sky");
         System.out.println(phr.substring(phr.indexOf(","),7));
     }
-} //, bl
+}
