15ecaa5b0f1e029fc3162d29eb33d3fe939d8ad8
Goh Yon Cing <55395418+Yoncing99@users.noreply.github.com>
2020-03-07 22:39:40 +0800

    Update Q7.java

--- Q7.java
+++ Q7.java
@@ -4,8 +4,10 @@ public class Q7{
         String text2 = "College of Arts and Sciences";
         String text3 = "UUM Sintok";
 
+        //a
         System.out.println(text1.length()+text2.length()+text3.length());
-        System.out.println("\""+text2.replace("Sciences","Computing")+"\"");
+        System.out.println(text2.substring(0,20) + text1.substring(10,19));
+       // System.out.println("\""+text2.replace("Sciences","Computing")+"\"");
     }
 } // 57
-  // "College of Arts and Computing"
+  // College of Arts and Computing
